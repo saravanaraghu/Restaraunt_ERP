518db8757b63d9d85c7d912d58ef67da97ee7077
RaghuswamySaravana Akshay <raghuswamysaravana.akshay@wpp.com>
2026-09-22 11:53:32 +0530

    Code is being changed

--- practice_python.py
+++ practice_python.py
@@ -1,1 +1,15 @@
-print("Hello World!")
+def calculate_bill(price, quantity):
+    total = price * quantity
+    return total
+
+def calculate_gst(bill_amount, gst_rate):
+    gst_amount = bill_amount * (gst_rate / 100)
+    return gst_amount
+
+price = 100
+quantity = 2
+bill_amount = calculate_bill(price, quantity)
+gst_rate = 5
+gst_amount = calculate_gst(bill_amount, gst_rate)
+print(f"Bill Amount: {bill_amount}")
+print(f"GST Amount: {gst_amount}")
